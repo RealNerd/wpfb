a6c125b0929d67b70523f4f5736e7aaa3703f002
aaroncollegeman <aaroncollegeman@911da2fc-602b-11de-84d6-ef71daef37f8>
2009-08-14 02:30:51 +0000

    Ready to build

--- src/wp-content/plugins/wpfb/connect.php
+++ src/wp-content/plugins/wpfb/connect.php
@@ -1,12 +1,39 @@
-<?php $wpfb_api_key = 'c3a61c0df2c8f21e4446424bfffa4193'; //get_option('wpfb.api_key'); ?>
+<?php 
+/**
+ * Wordpress Facebook Connect Integration Platform
+ * Copyright (C)2009 Collegeman.net, LLC.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. 
+ */
 
-<?php if ($wpfb_api_key): ?>
+$wpfb_comments_api_key = get_option('wpfb.comments_api_key'); 
+?>
+
+<?php if ($wpfb_comments_api_key): ?>
 	<div id="facebook-comment">
-		<h2><?php echo get_option("wpfb.comment_template_title", "Comments:") ?></h2>
-	 	<fb:comments width="<?php echo get_option('wpfb.box_width', '475px') ?>"></fb:comments>
+		<h2 style="padding-bottom: 10px;"><?php echo get_option("wpfb.comments_template_title", "Comments:") ?></h2>
+	 	<fb:comments 
+	 		xid="<?php wpfb_comment_xid() ?>"
+	 		numposts="<?php echo get_option('wpfb.comments_numposts', '10') ?>"
+	 		width="<?php echo get_option('wpfb.comments_width', '550px') ?>" 
+	 		css="<?php wpfb_comment_css_url() ?>"
+	 		reverse="<?php echo get_option('wpfb.comments_reverse', 'false') ?>"
+	 		quiet="<?php echo get_option('wpfb.comments_quiet', 'false') ?>"></fb:comments>
 		<script type="text/javascript">
 			jQuery(function() {
-  				FB.init('<?php echo $wpfb_api_key ?>', '<?php wpfb_xd_receiver_url() ?>');
+  				FB.init('<?php echo $wpfb_comments_api_key ?>', '<?php wpfb_xd_receiver_url() ?>');
 			});
 		</script>
 	</div>
